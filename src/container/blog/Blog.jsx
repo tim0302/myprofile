735550a059342b216fcad9d35270941e1c3a995b
102419310 <54136440+tim0302@users.noreply.github.com>
2021-12-27 22:03:43 +1100

    all files (tutorial)

---
+++ src/container/blog/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './blog.css';
+function Blog() {
+    return (
+        <div>
+            Article
+        </div>
+    )
+}
+
+export default Blog
---
+++ src/container/features/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './features.css';
+function Features() {
+    return (
+        <div>
+            Article
+        </div>
+    )
+}
+
+export default Features
