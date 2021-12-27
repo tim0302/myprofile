399d757da3ca56006e27ca2e5d2783d6ccb67d91
102419310 <54136440+tim0302@users.noreply.github.com>
2021-12-27 22:31:23 +1100

    All Files setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from './containers';
+import { CTA, Brand, Navbar } from './components';
 function App() {
   return (
-    <div>
-      <h1>Under construction.</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css';
+function Header() {
+    return (
+        <div>
+            Article
+        </div>
+    )
+}
+
+export default Header
