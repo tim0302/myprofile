4916c53b858f1b31da29d4b73463c82d7ae27c0c
102419310 <54136440+tim0302@users.noreply.github.com>
2021-12-31 00:36:54 +1100

    header done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Header,
 } from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 function App() {
   return (
     <div className='App'>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import dusk from '../../assets/dusk.png';
+
 function Header() {
     return (
-        <div>
-            Article
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">
+                    Let's Build Our demonstration website with amazing React
+                </h1>
+                <p>Why are you reading this. Why are you reading this. Why are you reading this. Why are you reading this. Why are you reading this. Why are you reading this. Why are you reading this. Why are you reading this. Why are you reading this. Why are you reading this. Why are you reading this.</p>
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your email address"></input>
+                    <button type="button">Get started!</button>
+                </div>
+
+                <div className="gpt3__header-content__people">
+                    <img src={people}/>
+                    <p>1,600 people requested access to this site.</p>
+                </div>
+                
+            </div>
+            <div className="gpt3__header-image">
+                    <img src={dusk} alt="dusk"/>
+                </div>
         </div>
     )
 }
