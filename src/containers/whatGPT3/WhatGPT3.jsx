7547a2875d249a039f74268f6860bf8bbc9f9dc2
102419310 <54136440+tim0302@users.noreply.github.com>
2022-01-03 04:30:15 +1100

    final

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 function WhatGPT3() {
-    return (
-        <div>
-            Article
-        </div>
-    )
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature
+          title='What is this shiet'
+          text='RUUUUU UUUUAAAAAAA AAAAAAAA AAAAAAAAAAAAAA AAAAAAAAAA  AAAAAAAA AAAAAAAAAAAAAAAAAAA'
+        />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>
+          This is the heading and we should probably try a larger version.
+        </h1>
+        <p>Explore this whole shting</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature
+          title='What is this shiet'
+          text='RUUUUUUUUU AAAA AAAAAAAAA AAAAAAAAAA AAAAAAAA AAAAAAAA AAAAA AAAAAAAAA AAAAAAAAAAAAA'
+        />
+        <Feature
+          title='What is this shiet'
+          text='RUUUUU UUUUAAAAAA AAAAAAA AAAAA AAAAAAAAA AAAAAAAAAA AAAA AAAAAAAAAA AAAAA AAAAAAAAAA'
+        />
+        <Feature
+          title='What is this shiet'
+          text='RUU UUUUU UUAAAAAA AAAAA AAAAAAAA AAAAAAAAAAA AAAAAAAAAA AAAAAAAAAA AAAAAAAAAAA AAAAA'
+        />
+        <Feature
+          title='What is this shiet'
+          text='RUUUUUUUUUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+        />
+      </div>
+    </div>
+  );
 }
 
-export default WhatGPT3
+export default WhatGPT3;
